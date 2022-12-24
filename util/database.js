01e0bfe1c1efe6c6d7f9716755cf01f9adb8bcfc
sadanandprabhu7 <sadanandprabhu7@gmail.com>
2022-12-24 06:11:19 +0530

    JENKINS TEST

--- util/database.js
+++ util/database.js
@@ -6,11 +6,11 @@ env.config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
-  process.env.DB_USER_NAME,
+  process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
     dialect: "mysql",
-    host: process.env.HOST_NAME,
+    host: process.env.DB_HOST,
   }
 );
 
